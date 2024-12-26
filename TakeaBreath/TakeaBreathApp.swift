1357d2d087dcf2977eda72e61bef2bfaad8e7aa1
Mehmet Kamay <mehmetkamay506@gmail.com>
2024-12-26 17:36:02 +0300

    Initial Commit

---
+++ TakeaBreath/TakeaBreathApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeaBreathApp.swift
+//  TakeaBreath
+//
+//  Created by Mehmet Kamay on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TakeaBreathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TakeaBreathTests/TakeaBreathTests.swift
@@ -0,0 +1,17 @@
+//
+//  TakeaBreathTests.swift
+//  TakeaBreathTests
+//
+//  Created by Mehmet Kamay on 26.12.2024.
+//
+
+import Testing
+@testable import TakeaBreath
+
+struct TakeaBreathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
